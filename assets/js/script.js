2061680ebcf61a961234ef0b2517b74bdb152c38
Brandon Norsworthy <brandonknorsworthy@gmail.com>
2021-07-05 13:23:52 -0500

    bringing back animal object from api

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,16 +1,27 @@
 
-secret = `5FxTpncHn5lzFXtfQykl1xpBtDX1O3q6QC8KWhrS`
-apiKey = `8iY0rDCqC9P4DVIu9eTVbZY5cUJW1QoDmuRxes5N6FQi72MxhF`
-zip = 77388
-url = `https://api.petfinder.com/v2/types`
+//global variables
+const secret = `5FxTpncHn5lzFXtfQykl1xpBtDX1O3q6QC8KWhrS`
+const apiKey = `8iY0rDCqC9P4DVIu9eTVbZY5cUJW1QoDmuRxes5N6FQi72MxhF`
+const url = `https://api.petfinder.com/v2/types`
 
-var pf = new petfinder.Client({apiKey: "8iY0rDCqC9P4DVIu9eTVbZY5cUJW1QoDmuRxes5N6FQi72MxhF", secret: "5FxTpncHn5lzFXtfQykl1xpBtDX1O3q6QC8KWhrS"});
+//string must either be `city, state` or `zipcode`
+var userLocation = `austin, texas`
 
-pf.animal.search()
-    .then(function (response) {
-        // Do something with `response.data.animals`
-        console.log(response)
+function petfinderCall() {
+    var pf = new petfinder.Client({ apiKey: apiKey, secret: secret });
+    pf.animal.search({
+        location: userLocation,
+        type: "dog",
+        distance: 15
     })
-    .catch(function (error) {
-        // Handle the error
-    });
+        .then(function (response) {
+            //response object from api
+            console.log(response.data.animals[0]);
+        })
+        .catch(function (error) {
+            // Handle the error
+            console.log(error);
+        });
+}
+
+petfinderCall()
